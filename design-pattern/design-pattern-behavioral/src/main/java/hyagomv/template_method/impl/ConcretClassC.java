a47d27bf134af875769a1b72ad3cc86f4c493191
Hyago M. Vale <hyagomvale@gmail.com>
2020-09-15 02:42:04 -0300

    Template Method

---
+++ design-pattern/design-pattern-behavioral/src/main/java/hyagomv/template_method/impl/ConcretClassC.java
@@ -0,0 +1,10 @@
+package hyagomv.template_method.impl;
+
+public class ConcretClassC extends BaseClasse {
+
+	@Override
+	protected void stepOne() {
+		System.out.println("Etapa UM do Algoritmo");
+	}
+
+}
